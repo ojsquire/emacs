2b3fe04ed3640a69cb1ad6991df9ecc1ead02421
Oliver Squire <oliver.squire@booking.com>
2020-07-10 14:37:21 +0200

    Adds jedi things

--- init.el
+++ init.el
@@ -22,7 +22,6 @@
 (setq package-list
       '(exec-path-from-shell
         async
-        auto-complete
         bind-key
         helm
         jedi
@@ -126,6 +125,8 @@
 (setq backup-directory-alist `(("." . "~/.emacs_backups")))
 (setq backup-by-copying t)
 
+
+;; CUSTOM SET VARIABLES
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -140,3 +141,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;; JEDI
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
